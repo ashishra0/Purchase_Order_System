bfff5beca3dcca5dd71aa480d78a6d9cbde6970f
Ashish Rao <ashishrao2598@gmail.com>
2018-09-23 10:01:14 +0530

    Added footer file

---
+++ footer.php
@@ -0,0 +1,21 @@
+
+  <head>
+    <?php
+      include('resources.html')
+    ?>
+    <style>
+      em {
+        color: deeppink;
+      }
+    </style>
+  </head>
+
+<footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+      <strong>Purchase Order System</strong> By <em>Group 4</em>. The website content
+      is licensed by <em>Ashish Rao & Group</em>.
+      <p><?php echo date("Y-m-d"); ?></p>
+      </p>
+    </div>
+  </footer>
